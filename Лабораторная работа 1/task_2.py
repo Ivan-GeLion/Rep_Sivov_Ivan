eff940c58a891a0426bee5f39ec2caf362353f40
Ivan-GeLion <civovivan@gmail.com>
2024-11-22 18:14:20 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -7,4 +7,5 @@ size_token = 4  # байта
 size_disk_b = size_disk_mb * 1024 ** 2
 size_book = pages_in_book * lines_of_pages * token_lines * size_token
 max_book_in_disk = size_disk_b // size_book
+
 print(f"Количество книг, помещающихся на дискету: {(max_book_in_disk):.0f}")
